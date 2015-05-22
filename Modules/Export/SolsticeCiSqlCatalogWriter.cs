673cac540ac2eeebee60a555abbbe73c0bd5869a
unknown <RAOC7395@P-7ZZ11EEZ.rd.francetelecom.fr>
2015-05-22 22:29:44 +0200

    Files initialization.

---
+++ Modules/Export/SolsticeCiSqlCatalogWriter.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Babel Meta
+ * Copyright 2015 - Romain Carbou
+ * romain.carbou@solstice-music.com
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabelMeta.Modules.Export
+{
+    public class SolsticeCiSqlCatalogWriter : ICatalogWriter
+    {
+        public AppConfig.ReturnCodes Generate(string folder, MainFormViewModel viewModel = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
